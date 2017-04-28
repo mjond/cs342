85f6cc2463f0bb344d82e4c475a5979291f32c6d
Mark Davis <mjd85@students.calvin.edu>
2017-04-28 13:17:24 -0400

    Add files via upload

--- 11postrelational/Lab11/src/HelloWorld.java
+++ 11postrelational/Lab11/src/HelloWorld.java
@@ -1,7 +1,12 @@
 /**
  * Created by mjd85 on 4/21/2017.
  */
+import jdk.nashorn.internal.objects.annotations.Getter;
+
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
@@ -15,5 +20,29 @@ public class HelloWorld {
     public String getClichedMessage() {
         // Return some cliched textual content
         return "Hello World";
+    }
+
+    @Get
+    @Produces("text/plain")
+    public String getFunction() {
+        return "Getting...";
+    }
+
+    @Put
+    @Produces("text/plain")
+    public String putFunction(int input) {
+        return "Putting: " + input;
+    }
+
+    @Post
+    @Consumes("text/plain")
+    public String putFunction(String input) {
+        return "Posting: " + input;
+    }
+
+    @Delete
+    @Produces("text/plain")
+    public String putFunction(int input) {
+        return "Deleting: " + input;
     }
 }
